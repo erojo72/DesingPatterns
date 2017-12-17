0657bf4d581b671ec7e59352db4c547622e4e007
Emilio Rojo <emilio_rojo_72@hotmail.com>
2017-12-17 20:23:08 +0100

    Strategy Pattern

---
+++ Strategy/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Context
+    {
+        private IStrategy Strategy { get; set; }
+
+        public Context(IStrategy strategy)
+        {
+            Strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            Strategy = strategy;
+        }
+
+        public void ExecuteStrategy()
+        {
+            Strategy.Execute();
+        }
+
+
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStrategyA());
+
+            context.ExecuteStrategy();
+
+            context.SetStrategy(new ConcreteStrategyB());
+
+            context.ExecuteStrategy();
+
+            context.SetStrategy(new ConcreteStrategyC());
+
+            context.ExecuteStrategy();
+
+            Console.ReadLine();
+        }
+    }
+}
